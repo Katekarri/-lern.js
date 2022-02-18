f03714588c3dc856472d55918cee91c437ee64a6
Katti <kaminskaya.k2@gmail.com>
2022-02-18 20:30:47 +0100

    add 002 file

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function is_authoried() {
+
+}
+let authoried = is_authoried()
+
+let activeClass 
+if (authoried) {
+  activeClass = 'active1'
+} else {
+  activeClass = 'active2'
+}
+
+activeClass = authoried ? 'active1' : 'active2';
+
+
+
---
+++ Новая папка/002.js
@@ -0,0 +1,41 @@
+// task 1
+
+let a = 123;
+alert(a)
+
+// task 2(a)
+
+let a = 1;
+let b = 2;
+let c = 3;
+alert(a)
+alert(b)
+alert(c)
+
+// task 2(b)
+
+let a = 1, b = 2, c = 3;
+alert(a)
+alert(b)
+alert(c)
+
+// task 2(c)
+
+let a, b, c;
+a = 1;
+b = 2;
+c = 3;
+alert(a)
+alert(b)
+alert(c)
+
+// task 3
+
+let a;
+a = 10;
+alert(a)
+
+a = 20;
+alert(a)
+
+

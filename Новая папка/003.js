523ec91bf51f1fb9ac04c8fcdd6b3e31288c64eb
Katti <kaminskaya.k2@gmail.com>
2022-02-18 21:26:48 +0100

    add 003 file

---
+++ Новая папка/003.js
@@ -0,0 +1,82 @@
+// task 3
+
+let a = 10;
+let b = 2;
+alert(a + b)
+alert(a - b)
+alert(a * b)
+alert(a / b)
+
+// task 6
+
+let a = 10;
+let b = 5;
+let c = a - b;
+let d = 7;
+let result = c + d;
+alert(result)
+
+// task 7
+20
+
+// task 8
+23
+
+// task 9
+6
+
+// task 10
+9
+
+// task 11
+8
+
+// task 12
+8
+
+// task 13
+25
+
+// task 14
+13
+
+// task 15
+36
+
+// task 16
+4
+
+// task 17
+4
+
+// task 18
+4 
+
+// task 21
+
+let a = 11;
+alert(-a)
+
+// task 22
+
+let a = 10;
+let b = 3;
+alert(a % b)
+
+// task 24
+24
+
+// task 25
+216
+
+// task 26
+48
+
+// task 27
+24
+
+// task 28
+72
+
+
+

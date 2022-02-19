cb0b3f2db316fe12560c4d021d3aa538e6a80b8c
Katti <kaminskaya.k2@gmail.com>
2022-02-19 21:27:02 +0100

    add file 004

---
+++ Новая папка/004.js
@@ -0,0 +1,19 @@
+// task 3
+
+let str1 = 'hello';
+let str2 = 'world';
+alert(str1 + ' ' + str2)
+
+// task 4
+
+let str = 'jnkmloniugyg';
+alert(str.length)//12
+
+// task 5
+
+let str = `a
+b
+c`;
+alert(str)
+
+

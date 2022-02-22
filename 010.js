7edb50f44a62da5816f8ce84142e596fee24acba
Katti <kaminskaya.k2@gmail.com>
2022-02-22 12:39:30 +0100

    add file

---
+++ 010.js
@@ -0,0 +1,58 @@
+// task 1
+
+let a = '10';
+let b = '20';
+alert(Number(a) + Number(b));
+
+let a = '10';
+let b = '20';
+alert(Number(a + b));// будет ли так работать?
+
+// task 2
+5
+// task 3
+5
+
+// task 4
+'23'
+// task 5
+
+let a = +'2';
+let b = +'3';
+alert(a + b); 
+
+// task 6
+
+let a = parseInt('5px');
+let b = parseInt('6px');
+alert(a + b);
+
+// task 7
+
+let a = parseFloat('5.5px');
+let b = parseFloat('6.25px');
+alert(a + b);
+
+// task 8
+
+let a = parseFloat('5.5px');
+let b = parseFloat('6.25px');
+let c = 'px'
+alert(a + b + c);//coмневаюсь в ответе
+
+// task 9
+
+let num1 = String(18);
+let num2 = String(7);
+alert(num1 + num2);
+
+// task 10
+
+let num = 56;
+alert(String(num).length)
+
+// task 11
+
+let num1 = 14;
+let num2 = 96;
+alert(String(num1).length + String(num2).length)

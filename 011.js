0b0a8d6e1d9f30ea65641933a5d68b5c42bbea87
Katti <kaminskaya.k2@gmail.com>
2022-02-25 07:44:19 +0100

    add 011 and 012 file

---
+++ 011.js
@@ -0,0 +1,20 @@
+// task 1
+4
+
+// task 2
+2
+
+// task 3
+0
+
+// task 4
+1
+
+// task 5
+'1true'
+
+// task 6
+'true1'
+
+// task 7
+'true1'
---
+++ 012.js
@@ -0,0 +1,62 @@
+// task 1
+true
+
+// task 2
+false
+
+// task 3
+true 
+
+// task 4
+false
+
+// task 5
+false
+
+// task 6
+true
+
+// task 7
+false
+
+// task 8
+true
+
+// task 9
+true
+
+// task 10
+false
+
+// task 11
+true
+
+// task 12
+true
+
+// task 13
+false
+
+// task 14
+true
+
+// task 15
+false
+
+// task 16
+true
+
+// task 17
+false
+
+// task 18
+true
+
+// task 19
+true//?
+
+// task 20
+true
+
+// task 21
+true

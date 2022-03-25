c292c9408111d13501b0fc7c53ac7156577277b4
Katti <kaminskaya.k2@gmail.com>
2022-03-25 08:32:31 +0100

    add file array

---
+++ array.js
@@ -0,0 +1,36 @@
+// 1. У вас есть 5 чисел. Найдите наибольшее и наименьшее из них
+
+// 2. У вас есть массив
+// const arr = [ "Hi", "Hello", "Bonjour"];
+// Добавьте к нему еще один элемент в конец, чтобы получился следующий массив
+// [ "Hi", "Hello", "Bonjour", "Hola"]
+
+const arr = ["Hi", "Hello", "Bonjour"];
+arr[3] = "Hola";
+// console.log(arr);
+
+const arr2 = ["Hi", "Hello", "Bonjour"];
+arr2.push("Hola");
+console.log(arr2);
+
+// 3. Есть 2 массива
+// сonst a = ['a', 'b', 'c'];
+// const b = [1, 2, 3];
+// Объедините их с помощью цикла, чтобы получилось
+// ['a', 'b', 'c', 1, 2, 3]
+
+// 4. Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1]
+
+// 5. У нас есть некий массив с данными:
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// Проверьте, есть ли в этом массиве значение Banana
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+for (i = 0; i < fruits.length; i++){
+  if (fruits[0]) { 
+  }    
+}
+// console.log("Banana")
+
+// 6. Удалите из этого массива элемент 5.
+// const array = [2, 5, 9];

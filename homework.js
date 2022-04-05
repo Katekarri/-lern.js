8e8e92e56db9629127e7dca0148620d1f8ca0a3e
Katti <kaminskaya.k2@gmail.com>
2022-04-05 07:44:49 +0200

    add tasks in file homework

--- homework.js
+++ homework.js
@@ -26,7 +26,7 @@ for (i = 0; i < a.length; i++){
    }
   }
 }
-console.log(arrayOfCommonElement)
+console.log(arrayOfCommonElement);
 
 
 // Задача 3
@@ -41,29 +41,35 @@ console.log(arrayOfCommonElement)
 // Сортировка слиянием
 // Пирамидальная сортировка
 
-// let example2 = [19, 8, -93, 4, 1, 16, 77, 780, 43, 237, 56];
-// let arr2 = example2.sort((a, b) => a-b);
-// console.log(arr2);
-// console.log(arr2.reverse());
-
-
-let example2 = [19, 8, -93, 4, 1, 16, 77, 780, 43, 237, 56];
-for (i = 0; i < example2.length; i++) {
-  for (let j = i; j < example2.length; j++) {
-    for (let j = i; j < item.length; j++) {
-      if (example2[j] > example2[i]) {
-        [example2[i], example2[j]] = [example2[j], example2[i]];
-      }
-    }
-  }
-}
-
+ let example2First = [19, 8, -93, 4, 1, 16, 77, 780, 43, 237, 56]; 
+ let arr2 = example2First.sort((a, b) => a-b);
+ console.log(arr2);
+ console.log(arr2.reverse());
 
 
 // Задача 4
 // Выведите "Да"" если стрка полиндром и "Нет", если строка не является полиндромом. Палиндром — это слово или фраза, которые одинаково читаются слева направо и справа налево.
 
-//шалаш
+
+let example = 'шалаш';
+let w = 1;
+let count = 0;
+for (i = 0; i < (example.length - 1) / 2; i++){// length - 1 / 2(полоаина итеаций)  убираем букву по середине (не повторяюуюся в данном случае это 'л')
+  if ( example[i] == example[example.length - w]) {
+    count++;  // счетчик увеличиваем на 1
+    w++;
+   // увеличивает пеменную что бы передвинутся  на следуюую букву с конца
+  } else {
+    console.log('Не полиндромом');
+    break;
+  }
+ 
+  }
+ if (count == (example.length - 1) / 2) {
+  console.log('Полиндром');
+}
+
+
 
 // Задача 5
 // Вы принимаете от пользователя последовательность чисел, разделённых запятой, т.е строка состоящая из чисел и запятых. Составьте массив с этими числами.
@@ -108,7 +114,7 @@ for (i = 0; i < first.length; i++){
     }  
   } 
 }
- console.log(elementsFirst)
+console.log(elementsFirst);
   
 // Задача 9
 // Сложите цифры целого числа.
@@ -134,24 +140,24 @@ for (i = 0; i <= exampel10.length; i++){
   }
 }
 // numberOfTimes = numberOfTimes.length
- console.log(numberOfTimes)
+console.log(numberOfTimes);
 
 
 // Задача 11
 // Нужно проверить, все ли числа в массиве уникальны.
 
 let example11 = [19, 8, 93, 4, 1, 16, 1, 77, 780, 43, 237, 56];
-let uniqueElement = 0;
+let uniqueElement = 0;// уникальные значения 
 for (let i = 0; i < example11.length; i++){
-    let item = example11.indexOf(example11[i], i+1);  
-    if (item >= 0) {
+    let item = example11.indexOf(example11[i], i+1);// еременная которая выбиает елементы которые следуют за текуим элементом (по индексу)
+    if (item >= 0) {// если indexOf возвращает index повтовтоки (от 0 и выше) - значит повтока есть 
         console.log('Есть повторки');
         break;
-    } else {
-        uniqueElement++;
+    } else {// а иначе он возваает -1 что значит овторки не надены
+        uniqueElement++;// и в таком случае мы увеличиваем счетчик уникальных значений на 1
     }    
 }
-if(uniqueElement === example11.length) {
+if(uniqueElement === example11.length) {// если счетчик уникальных значений авен длинне массива - все значения уникальны 
     console.log('Все значения уникальны!');
 }
   
@@ -167,8 +173,8 @@ for (i = 0; i < example12.length; i++){
     maxNumber = example12[i];  
   }  
 }
-console.log(maxNumber)
-console.log(example12.indexOf(maxNumber))
+console.log(maxNumber);
+console.log(example12.indexOf(maxNumber));
 
 
 

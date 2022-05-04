d144f1f0d5cbe569667d7a12d3c7409fda27f330
Katti <kaminskaya.k2@gmail.com>
2022-05-04 21:37:57 +0200

    add files

--- 039.js
+++ 039.js
@@ -18,7 +18,7 @@ const obj2 = {
   c: 3,
 };
 
-let sumElement = (obj2.a + obj2.b + obj2.c);
+const sumElement = obj2.a + obj2.b + obj2.c;
 
 // Задача 3
 // Создайте объект с ключами 1a, 2b и с - с и значениями 1, 2 и 3. Найдите сумму его элементов.
@@ -29,7 +29,7 @@ const obj3 = {
   c: 3
 };
 
-let sumElementObj3 = (obj3['1a'] + obj3['2b'] + obj3['c']);
+const sumElementObj3 = obj3['1a'] + obj3['2b'] + obj3['c'];
 
 
 // Задача 4
@@ -37,7 +37,7 @@ let sumElementObj3 = (obj3['1a'] + obj3['2b'] + obj3['c']);
 // let obj = {'1a': 1, 'b2': 2, 'с-с': 3, 'd4': 4};
 // Для каких ключей данного объекта кавычки обязательны, а для каких нет ?
 
-'1a', b2, 'с-с', d4
+// '1a', b2, 'с-с', d4
  
 
 // Задача 5
@@ -46,7 +46,7 @@ let sumElementObj3 = (obj3['1a'] + obj3['2b'] + obj3['c']);
 // Найдите сумму его элементов.Обращайтесь к элементам с помощью изученного синтаксиса.
 
 const obj5 = {key1: 1, key2: 2, key3: 3};
-let sumElementObj5 = (obj5.key1 + obj5.key2 + obj5.key3);
+const sumElementObj5 = obj5.key1 + obj5.key2 + obj5.key3;
 
 
 // Задача 6
@@ -54,7 +54,7 @@ let sumElementObj5 = (obj5.key1 + obj5.key2 + obj5.key3);
 // let obj = {'1a': 1, 'b2': 2, 'eee-': 3, 'd4': 4};
 // К каким элементам этого объекта допустимо обращение через свойство, а к каким - нет ?
 
-obj6.b2, obj6.d4, obj6['1a'], obj6['eee-']
+// obj6.b2, obj6.d4, obj6['1a'], obj6['eee-']
 
 
 // Задача 7
@@ -65,8 +65,10 @@ const user = {
   surname: 'Sky',
   patronymic: 'Worker',
 }
-console.log(`${user.nameUser}` + ' ' + `${ user.surname}` + ' ' + `${ user.patronymic}`);
-
+console.log(`${user.nameUser}` + ' ' + `${user.surname}` + ' ' + `${user.patronymic}`);
+console.log(`${user.nameUser + ' ' + user.surname + ' ' + user.patronymic}`);
+console.log(`${user.nameUser} ${user.surname} ${user.patronymic}`);
+console.log(user.nameUser + ' ' + user.surname + ' ' + user.patronymic);
 
 // Задача 8
 // Создайте объект date с ключами year, month и day и значениями, соответствующими текущему дню.Выведите созданную дату на экран в формате год - месяц - день.
@@ -78,8 +80,12 @@ const date = {
   day: '03',
 }
 
-let today = `${date.year}` + '-' + `${ date.month}` + ' - ' + `${ date.day}`;
+let today = `${date.year}` + '-' + `${date.month}` + '-' + `${date.day}`;
 // let today = date.year + '-' + date.month + '-' + date.day;
 console.log(today);
+
+const x = [date.year, date.month, date.day].join('-');
+console.log(x);
+
 
 
--- 113.js
+++ 113.js
@@ -4,20 +4,12 @@ import { testMe } from './utils.js'
 // Сделайте функцию, которая параметром будет принимать массив с числами, и проверять, что все элементы в этом массиве являются четными числами.
 
 function checkNum(arr) {  
-  let finishArray = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 == 0) {
-          finishArray.push(arr[i]);
-          
-        } else { 
-          return false;
-          break;
-        }
-  } 
-   if(finishArray.length === arr.length) {
-    return true;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 != 0) { 
+      return false;
     }
+  } 
+  return true;    
 }
 
 checkNum([8, 6, 10, 4, 2])
@@ -44,35 +36,41 @@ testMe(checkNum, [1, 3, 5, 7, 11], false)
 // Сделайте функцию, которая параметром будет принимать число и проверять, что все цифры это числа являются нечетными.
 
 function checkNumbers(num){
-  let flag = true;
-  
-let str = String(num);
-  for (let i = 0; i < str.length; i++){
-  
-if(str[i] % 2 == 0){
-flag = false;
+  let areAllOdd = true;
+
+    const str = String(num);
+    for (let i = 0; i < str.length; i++) {
+      if(str[i] % 2 == 0) {
+      areAllOdd = false;
+      }  
     }
-    
-}return flag;
+
+    return areAllOdd;
 }
 checkNumbers(86)
 
 
+testMe(checkNumbers, 86 , false)
+testMe(checkNumbers, 21, false)
+testMe(checkNumbers, 111, true)
 
 // Задача 3
 // Сделайте функцию, которая параметром будет принимать массив и проверять, есть ли в этом массиве два одинаковых элемента подряд.
 
-function checkSameNumbers(arr){
-  let flag = false;
+function checkSameNumbers(arr) {
+  let hasDublicate = false;
   
-for(let i = 0; i < arr.length; i++){
-
-  if (arr[i] == arr[i + 1]) {
-    flag = true;
-    break;
+  for (let i = 0; i < arr.length-1; i++) {
+    if (arr[i] == arr[i + 1]) {
+      hasDublicate = true;
+      break;
+    }
   }
-  
-}return flag;
+
+  return  hasDublicate;
 }
 
-checkSameNumbers[19, 19, -93, 4, 1, 16, 77, 780, 43, 237, 56];
+checkSameNumbers[19, -93, 4, 1, 16, 77, 780, 43, 237, 56];
+
+testMe(checkSameNumbers, [19, -93, 4, 1, 16, 77, 780, 43, 237, 56], false)
+testMe(checkSameNumbers, [19, 19, 4, 1, 16, 77, 780, 43, 237, 56], true)

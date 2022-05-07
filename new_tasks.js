c7e729ff1d76d3e2f2eabe72c24a9a7f5f6f89cc
Katti <kaminskaya.k2@gmail.com>
2022-05-07 22:06:22 +0200

    add files

--- new_tasks.js
+++ new_tasks.js
@@ -5,7 +5,7 @@ function firstTask() {
     const arr = [3, 5, 8, 16, 20, 23, 50];
   const result = [];
   // Пишем решение вот тут
-  for (let i = 0; i <= arr.length; i++){
+  for (let i = 0; i < arr.length; i++){
     result[i] = arr[i];
   }    
     // Не трогаем

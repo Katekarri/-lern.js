f280ee9d3dba53db7b1fbef7da649e18ce4b3034
Katti <kaminskaya.k2@gmail.com>
2022-05-09 08:27:42 +0200

    add tasks

--- new_tasks.js
+++ new_tasks.js
@@ -1,8 +1,9 @@
+import { testMe } from './utils.js'
 //задача 1
 
 function firstTask() {
     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const arr = [3, 5, 8, 16, 20, 23, 50];
+  const arr = [3, 5, 8, 16, 20, 23, 50];
   const result = [];
   // Пишем решение вот тут
   for (let i = 0; i < arr.length; i++){
@@ -15,17 +16,20 @@ function firstTask() {
 
 //задача 2
 // Место для второй задачи
+
 function secondTask() {
     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
+  const data = [5, 10, 'Shopping', 20, 'Homework'];
     // Пишем решение вот тут
-  for (i = 0; i < data.length; i++) {
+  
+  for (let i = 0; i < data.length; i++) {
+    const currentElement = data[i];
+    
+    if (typeof currentElement === 'number') {
+      data[i] = currentElement * 2;
 
-    if (data[i] === Number.data) {
-      data[i] = data[i] * 2;
-
-    } else if(data[i] === String.data) {
-      data[i] = `${data[i]}-done`;
+    } else if(typeof currentElement === 'string') {
+      data[i] = `${currentElement}-done`;
     }
   } 
     // Не трогаем
@@ -33,22 +37,43 @@ function secondTask() {
 }
 
 secondTask();
+testMe(secondTask,[10, 20, 'Shopping-done', 40, 'Homework-done']);
+
 
 //задача 3
 // Место для третьей задачи
 function thirdTask() {
     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
+  const data = [5, 10, 'Shopping', 20, 'Homework'];
+  const result = [];
 
     // Пишем решение вот тут
-    
-    
+  for (let i = data.length - 1; i >= 0; i--) {
+    result.push(data[i]);
+  }  
     // Не трогаем
-    return result;
+  return result;
 }
+
+thirdTask()
+
+testMe(thirdTask, ['Homework', 20, 'Shopping', 10, 5])
+
+
 
 //задача 3***
 const lines = 5;
 let result = '';
 // Проверяется именно переменная result, формируйте строку в ней
+
+function paintStar(lines) {
+  
+}
+testMe(paintStar,6,[
+    [' ', ' ', ' ', ' ', ' ', '*', ' ', ' ', ' ', ' ', ' ',],
+    [' ', ' ', ' ', ' ', '*', '*', '*', ' ', ' ', ' ', ' ',],
+    [' ', ' ', ' ', '*', '*', '*', '*', '*', ' ', ' ', ' ',],
+    [' ', ' ', '*', '*', '*', '*', '*', '*', '*', ' ', ' ',],
+    [' ', '*', '*', '*', '*', '*', '*', '*', '*', '*', ' ',],
+    ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',],
+])

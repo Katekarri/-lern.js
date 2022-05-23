79524467586c001d78a5c335caea0a663d6c6885
Katekarri <kaminskaya.k2@gmail.com>
2022-05-23 06:32:00 +0100

    fixt tasks

--- 133.js
+++ 133.js
@@ -19,6 +19,7 @@
 // Задача 3
 // Создайте функцию как Function Declaration. Проверьте то, что она будет доступна выше места своего объявления.
 
+func1()
 function func1() {
   alert('hi')
 }
@@ -26,16 +27,17 @@ function func1() {
 // Задача 4
 // Создайте функцию как Function Expression. Проверьте то, что она не будет доступна выше места своего объявления.
 
+func1()
 const func2 = function () {
   alert('hi')
 };
 
 // Задача 5
 // Расставьте точки с запятой во всех необходимых местах:
-// let func1 = function() {alert('!')};//тут
+// let func1 = function() {alert('!')};
 // let func2 = function() {
 // 	alert('!')
-// };//тут
+// };
 // function func3() {
-// 	alert('!')
+// 	alert('!');
 // }

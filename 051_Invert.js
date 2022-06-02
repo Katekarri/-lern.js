5ed6375a44f4d78e1e17894f3668926403a3a5f0
Katekarri <kaminskaya.k2@gmail.com>
2022-06-02 08:31:30 +0200

    add fils 050.logical_operatory.js, 051_Invert.js, 052_boolean_else-if.js

---
+++ 051_Invert.js
@@ -0,0 +1,12 @@
+//https://code.mu/ru/javascript/book/prime/conditions/statements-inverting/
+
+// Задача 1
+// Дан следующий код:
+// if (num1 >= 0 || num2 <= 10) {
+// 	alert('верно');
+// } else {
+// 	alert('неверно');
+// }
+// Используя оператор ! инвертируйте приведенное условие.
+
+//( !(num1 >= 0 || num2 <= 10) )

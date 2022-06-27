cd86e759fbced7cf188caac534212482b041fe5a
Katekarri <kaminskaya.k2@gmail.com>
2022-06-27 17:33:02 +0200

    add file 091_arrays_with_string_accumulation.js

--- 090_conclusion_of_the_pyramids.js
+++ 090_conclusion_of_the_pyramids.js
@@ -34,6 +34,25 @@ for (let i = 1; i <= 5; i++) {
 // 7777777
 // 999999999
 
+// for (let i = 1; i <= 10; i+=2) {
+//   let str2 = '';
+//   for (let j = 1; j <= i; j++) {
+//     str2 += i;
+//   }
+//   console.log(str2);
+// }
+
+
+
+for (let i = 1; i <= 9; i+=2) {
+  let str = '';
+	
+	for (let j = 1; j <= i; j++) {
+		str += i;
+	}	
+	document.write('<br>' + str + '</br>');
+}
+ 
 
 
 // Задача 3
@@ -44,6 +63,14 @@ for (let i = 1; i <= 5; i++) {
 // xxxx
 // xx
 
+
+for (let i = 5; i >= 1; i--) {
+  let str3 = '';
+  for (let j = 0; j < i; j++) {
+    str3 += 'xx'
+  }
+  document.write('<br>' + str3 + '</br>');
+}
 
 
 // Задача 4
@@ -57,3 +84,15 @@ for (let i = 1; i <= 5; i++) {
 // 333
 // 22
 // 1
+
+
+for (let i = 9; i >= 1; i--) {
+  let  str4 = '';
+  for (let j = 0; j < i; j++) {
+    str4 += i;
+  }
+  console.log(str4);
+}
+
+
+

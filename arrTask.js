42eaf7c8d43d3724a8449da9dc06458e7e509464
Katekarri <kaminskaya.k2@gmail.com>
2022-12-15 18:48:19 +0100

    add tasks

--- arrTask.js
+++ arrTask.js
@@ -17,13 +17,8 @@ const films = [
       name: 'Some bad film',
       rating: 4
   }
-];
 
-function showGoodFilms(arr) {
-    return arr.filter(film => film.rating >= 8);
-}
-
-
+]
 
 // 2) Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую.
 
@@ -40,9 +35,14 @@ function showGoodFilms(arr) {
 
 
 
+// function showListOfFilms(films) { 
+//   let  movie = films.map(film2 => film2.name + ', ').join('');
+//   return  movie;
+// }
+
+
 function showListOfFilms(films) { 
-  let  movie = films.map(film2 => film2.name + ', ').join('');
-  return  movie;
+  return films.map(film => film.name).join(', ');
 }
 
 // 3) Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
@@ -54,8 +54,7 @@ function showListOfFilms(films) {
 
 function setFilmsIds(arr) {
   return arr.map((film, i) => {
-      film.id = i;
-      return film;
+      return {...film, id: i};
   });
 }
 
@@ -66,17 +65,38 @@ const tranformedArray = setFilmsIds(films);
 // 4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray); :)
 
 
+// setFilmsIds(films)  => [   { name: 'Titanic', rating: 9, id: 0 },   { name: 'Die hard 5', rating: 5, id: 1 },   { name: 'Matrix', rating: 8, id: 2 },   { name: 'Some bad film', rating: 4, id: 3 } ]
+
 // function checkFilms(arr) {
 //   return arr.every(film => film.id || film.id === 0 ? true : false)
+// }
+
+// function checkFilms2(arr) {
+//   return arr.every( elem =>  "id" in elem);       
 // }
 
 // checkFilms(tranformedArray);
 
 
-function checkFilms(arr) {
-  return arr.every(id in films);
-}
+// function checkFilms(arr) {
+//   return arr.every(function(elem) {
+//     console.log(elem);
+//     if ('id' in elem) {
+//       return true;
+//     } else {
+//       return false;
+//     }  
+//   });  
+// }
 
-checkFilms(tranformedArray);
+// function checkFilms2(arr) {
+//   return arr.every( elem =>  "id" in elem);       
+// }
+
+const checkFilms3 = arr => arr.every( elem =>  "id" in elem); 
+
+console.log(checkFilms3(tranformedArray));
+
+
 
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/in

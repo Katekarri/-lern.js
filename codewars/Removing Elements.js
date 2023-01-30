d982bc6667420eff0b8b07378041c9aed71d5c55
Katekarri <kaminskaya.k2@gmail.com>
2023-01-30 15:04:18 +0100

    add tasks

--- codewars/Removing Elements.js
+++ codewars/Removing Elements.js
@@ -1,12 +1,4 @@
 // Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
 //Возьмите массив и удалите каждый второй элемент из массива. Всегда сохраняйте первый элемент и начинайте удаление со следующего элемента.
 
-function removeEveryOther(arr){
-  const newArr = [];
-  for (let i = 0; i < arr.length; i++) { 
-    newArr.splice(i + 1);
-  }
-}
-
-removeEveryOther([1, 2, 3, 4, 5, 6, 7]);
-
+const removeEveryOther = arr => arr.filter((_, i) => i % 2 === 0)

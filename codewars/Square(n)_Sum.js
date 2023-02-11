07bc21e634b5690311da62b0ab9f47a25048de38
Katekarri <kaminskaya.k2@gmail.com>
2023-02-11 14:48:12 +0100

    add tasks

---
+++ codewars/Square(n)_Sum.js
@@ -0,0 +1,13 @@
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+//Завершите функцию квадратной суммы, чтобы она возводила в квадрат каждое переданное ей число, а затем суммировала результаты.
+
+function squareSum(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.lenght; i++) {    
+    sum += i**2;
+  }
+  return sum;
+}
+
+squareSum([1,2,3])

42034a9b0145f88de9b0b21e613ed30b31001233
Katekarri <kaminskaya.k2@gmail.com>
2023-02-20 12:11:26 +0100

    add tasks

--- codewars/is_it_a_digit.js
+++ codewars/is_it_a_digit.js
@@ -1,6 +1,6 @@
 //Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
 
-//Реализовать String#digit? (в Java StringUtils.isDigit(String)), который должен возвращать true, если данный объект является цифрой (0-9), иначе false.
+//Реализовать String#digit? (в Java StringUtils.isDigit(String)), который должен возвращать true, если данный объект является цифрой (0-9), иначе false
 
 String.prototype.digit = function(num) {
   if (num = 0 && num < 10 ) {

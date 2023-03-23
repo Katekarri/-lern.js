d8f9032b8df4176d78b0740fb4ccc25b899fea55
Katekarri <kaminskaya.k2@gmail.com>
2023-03-23 11:29:13 +0100

    add tasks

---
+++ codewars/tip_calculator.js
@@ -0,0 +1,43 @@
+//https://www.codewars.com/kata/56598d8076ee7a0759000087/train/javascript
+
+//The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return
+//"Rating not recognised" in Javascript, Python and Ruby...
+//...or null in Java
+//...or -1 in C#
+//Because you're a nice person, you always round up the tip, regardless of the service.
+
+//Рейтинг нечувствителен к регистру (поэтому "отлично" = "ОТЛИЧНО"). Если получен непризнанный рейтинг, то нужно вернуть:
+//«Рейтинг не распознан» в Javascript, Python и Ruby...
+//... или ноль в Java
+//... или -1 в С#
+//Поскольку вы хороший человек, вы всегда округляете чаевые, независимо от услуги.
+
+function calculateTip(amount, rating) {
+
+  if (rating === "Terrible") { 
+    return Math.ceil(amount  * 0 / 100);
+  }
+  else if (rating === "Poor") {
+    return Math.ceil(amount * 5 / 100);
+  }
+  
+  else if (rating === "Good") {
+    return Math.ceil(amount * 10 / 100);
+  }
+  
+  else if (rating === "Great") {
+    return Math.ceil(amount * 15 / 100);
+  }
+  
+  else if (rating === "Excellent") {
+    return Math.ceil(amount * 20 / 100);
+  }
+    
+  else {
+    return ("Rating not recognised");
+  }
+     
+  }
+
+
+
